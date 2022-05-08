18768d9def2ee1bedce8929e1625e0865e685acc
Nicholas Avino <nckavino0903@gmail.com>
2022-05-08 17:27:36 -0400

    commit tree d7a6df4a262ade861c8bef98c647385fd89b8868

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+const thankYou = [];
+
+function writeCards(string, event) {
+    for (let i = 0; i < string.length; i++) {
+        thankYou.push(`Thank you, ${string[i]}, for the wonderful ${event} gift!`);
+
+    }
+
+    return thankYou;
+}
+
+writeCards();
+
+function countDown(int) {
+   let count = 0;
+   while (count <= int) {
+       console.log(int--);
+   }
+
+}
+
